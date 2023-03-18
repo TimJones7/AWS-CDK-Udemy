36eca9dd9fed85819f6996d938c79d874630c8fb
Tim Jones <timothy.jones@gmx.us>
2023-03-18 17:43:17 -0400

    section 2 finished

--- Section-2-10/lib/section-2-10-stack.ts
+++ Section-2-10/lib/section-2-10-stack.ts
@@ -1,22 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
-import { CfnParameter } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { MyBucket } from '../resources/s3_buckets/MyBucket';
 
 // This defines one stack
 export class Section210Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    
     super(scope, id, props);
 
-    // Adding parameters to stack from cli
-    const duration = new CfnParameter(this, 'duration', {
-      type: 'Number',
-      default: 5,
-      minValue: 2,
-      maxValue: 10
-    })
-
-    const myBucket1 = new MyBucket(this, 'MyAbstractedBucket', 'myBucket1', duration.valueAsNumber);
-    
+    // Defines a resource on the stack
+    const myBucket1 = new MyBucket(this, 'MyAbstractedBucket', 'myBucket1');    
   }
 }
--- Section-2-10/resources/s3_buckets/MyBucket.ts
+++ Section-2-10/resources/s3_buckets/MyBucket.ts
@@ -1,20 +1,34 @@
-import { CfnOutput, Duration, Stack } from 'aws-cdk-lib';
+import { CfnOutput, CfnParameter, Duration, Stack } from 'aws-cdk-lib';
 import { Bucket } from "aws-cdk-lib/aws-s3";
 
+// Creating custom implementation of resource
 export class MyBucket extends Bucket{
-    constructor(stack: Stack, name: string, objectName: string, duration: number){
-        super(stack, name, {
-            lifecycleRules: [
-               {
-                 expiration: Duration.days(duration)
-               }
-            ]                 
-        });
+    constructor(stack: Stack, name: string, objectName: string){
+        
+      // Adding parameters to stack from cli
+        const duration = new CfnParameter(stack, 'duration', {
+          type: 'Number',
+          default: 5,
+          minValue: 2,
+          maxValue: 10
+        });  
 
+        // Abstract out specific resource props
+        const resourceProps = {
+          lifecycleRules: [
+             {
+               expiration: Duration.days(duration.valueAsNumber)
+             }
+          ]                 
+        };
+
+        // Call resource parent constructor to build resouce on stack
+        super(stack, name, resourceProps);
+
+        // Provide some cmd line output for resources created.
         new CfnOutput(stack, objectName, {
           value: this.bucketName 
         });
-
    }
 }
 
